f4bebb06576bd3beb93bf2c697d42404579a3ac3
sff1019 <starfield1019@gmail.com>
2019-08-03 16:05:02 +0900

    Compare single and multiple

--- net.py
+++ net.py
@@ -21,40 +21,28 @@ class MLP(chainer.Chain):
 
 
 class ParallelMLP(chainer.Chain):
-    def __init__(self, n_units, n_out, gpu0, gpu1, gpu2, gpu3):
+    def __init__(self, n_units, n_out, gpu0, gpu1):
         self.gpu0 = gpu0
         self.gpu1 = gpu1
-        self.gpu2 = gpu2
-        self.gpu3 = gpu3
 
-        super(ParallelMLPm, self).__init__()
+        super(ParallelMLP, self).__init__()
         with self.init_scope():
             self.mlp1_gpu0 = MLP(n_units // 2, n_units).to_gpu(self.gpu0)
             self.mlp1_gpu1 = MLP(n_units // 2, n_units).to_gpu(self.gpu1)
-            self.mlp1_gpu2 = MLP(n_units // 2, n_units).to_gpu(self.gpu2)
-            self.mlp1_gpu3 = MLP(n_units // 2, n_units).to_gpu(self.gpu3)
 
             self.mlp2_gpu0 = MLP(n_units // 2, n_units).to_gpu(self.gpu0)
             self.mlp2_gpu1 = MLP(n_units // 2, n_units).to_gpu(self.gpu1)
-            self.mlp2_gpu2 = MLP(n_units // 2, n_units).to_gpu(self.gpu2)
-            self.mlp2_gpu3 = MLP(n_units // 2, n_units).to_gpu(self.gpu3)
 
     def forward(self, x):
         z0 = self.mlp1_gpu0(x)
         z1 = self.mlp1_gpu1(F.copy(x, self.gpu1))
-        z2 = self.mlp1_gpu2(F.copy(x, self.gpu2))
-        z3 = self.mlp1_gpu3(F.copy(x, self.gpu3))
 
-        h0 = F.relu(z0 + F.copy(z1, gpu_0) + F.copy(z2, gpu_0), F.copy(z3, gpu_0))
-        h1 = F.relu(z1 + F.copy(z0, gpu_1) + F.copy(z2, gpu_1), F.copy(z3, gpu_1))
-        h2 = F.relu(z2 + F.copy(z0, gpu_2) + F.copy(z1, gpu_2), F.copy(z3, gpu_2))
-        h3 = F.relu(z3 + F.copy(z0, gpu_3) + F.copy(z1, gpu_3), F.copy(z2, gpu_3))
+        h0 = F.relu(z0 + F.copy(z1, self.gpu0))
+        h1 = F.relu(z1 + F.copy(z0, self.gpu1))
 
         y0 = self.mlp2_gpu0(h0)
         y1 = self.mlp2_gpu1(h1)
-        y2 = self.mlp2_gpu2(h2)
-        y3 = self.mlp2_gpu3(h3)
 
-        y = y0 + F.copy(y1, gpu_0) + F.copy(y2, gpu_0) + F.copy(y3, gpu_0)
+        y = y0 + F.copy(y1, self.gpu0)
 
         return y
